abce6cbb77a9bd22431b421d333e531be1e35c8f
Hai Nguyen <nguyenhohai88@gmail.com>
2020-05-19 16:01:09 -0500

    conver thumb video

--- ffmpeg-convert-video/functions/index.js
+++ ffmpeg-convert-video/functions/index.js
@@ -28,8 +28,8 @@ const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 ffmpeg.setFfmpegPath(ffmpegPath);
 
 // Max height and width of the thumbnail in pixels.
-const THUMB_MAX_HEIGHT = 200;
-const THUMB_MAX_WIDTH = 200;
+const THUMB_MAX_HEIGHT = 300;
+const THUMB_MAX_WIDTH = 300;
 // Thumbnail prefix added to file names.
 const THUMB_PREFIX = 'thumb_';
 
@@ -57,7 +57,7 @@ exports.generateThumbnail = functions.storage.object().onFinalize(async (object)
   // Download file from bucket.
   const file = bucket.file(filePath);
   await file.download({ destination: tempLocalFile });
-  //console.log('The file has been downloaded to', tempLocalFile);
+  console.log('The file has been downloaded to', tempLocalFile);
 
   // Exit if this is triggered on a file that is not an image.
   if (contentType.startsWith('image/')) {
@@ -65,54 +65,83 @@ exports.generateThumbnail = functions.storage.object().onFinalize(async (object)
     if (fileName.startsWith(THUMB_PREFIX)) {
       return;
     }
+    console.log('Enter converting image');
     const thumbFilePath = path.normalize(path.join(fileDir, `${THUMB_PREFIX}${fileName}`));
     const tempLocalThumbFile = path.join(os.tmpdir(), thumbFilePath);
     // Cloud Storage files.
-
     const metadata = {
       contentType: contentType,
       'Cache-Control': 'public,max-age=3600',
     };
     // Generate a thumbnail using ImageMagick.
     await spawn('convert', [tempLocalFile, '-thumbnail', `${THUMB_MAX_WIDTH}x${THUMB_MAX_HEIGHT}>`, tempLocalThumbFile], { capture: ['stdout', 'stderr'] });
-    //console.log('Thumbnail created at', tempLocalThumbFile);
+    console.log('Thumbnail created at', tempLocalThumbFile);
     // Uploading the Thumbnail.
     await bucket.upload(tempLocalThumbFile, {
       destination: thumbFilePath,
       public: true,
       metadata: metadata
     });
-    //console.log('Thumbnail uploaded to Storage at', thumbFilePath);
+    console.log('Thumbnail image uploaded to Storage at', thumbFilePath);
     fs.unlinkSync(tempLocalThumbFile);
     fs.unlinkSync(tempLocalFile);
   } else if (contentType.startsWith('video/')) {
     // This is to prevent triggering another converting mp4 file after already convert
-    var codec = await getCodec(tempLocalFile);
-    console.log("Codec of file is: " + codec);
-    if (codec === "h264") {
-      return;
-    }
     if (fileName.endsWith('_output.mp4')) {
       return;
     }
-    const mp4FilePath = path.normalize(path.join(fileDir, fileName.replace(/\.[^/.]+$/, '') + '_output.mp4'));
-    const targetTempFilePath = path.join(os.tmpdir(), mp4FilePath);
-    await convertFile(tempLocalFile, targetTempFilePath);
-    await bucket.upload(targetTempFilePath, {
-      destination: mp4FilePath,
-      uploadType: 'media',
-      resumable: false,
-      public: true,
-      metadata: { gzip: true, cacheControl: "public, max-age=31536000" }
-    });
-    //console.log('Mp4 uploaded to Storage at', mp4FilePath);
-    // Once the image has been uploaded delete the local files to free up disk space.
-    fs.unlinkSync(targetTempFilePath);
+    var codec = await getCodec(tempLocalFile);
+    console.log("Codec of file " + tempLocalFile + " is: " + codec);
+    // we only convert video which has codec not h264
+    if (codec !== "h264") {
+      const mp4FilePath = path.normalize(path.join(fileDir, fileName.replace(/\.[^/.]+$/, '') + '_output.mp4'));
+      const targetTempFilePath = path.join(os.tmpdir(), mp4FilePath);
+      await convertFile(tempLocalFile, targetTempFilePath, '100%');
+      await bucket.upload(targetTempFilePath, {
+        destination: mp4FilePath,
+        uploadType: 'media',
+        resumable: false,
+        public: true,
+        metadata: { gzip: true, cacheControl: "public, max-age=31536000" }
+      });
+      //console.log('Mp4 uploaded to Storage at', mp4FilePath);
+      // Once the image has been uploaded delete the local files to free up disk space.      
+      fs.unlinkSync(targetTempFilePath);
+    }
+
+    var dimensions = await getDimentions(tempLocalFile);
+    //create thumb for video
+    var reduceHeightPercentage = 0,
+      reduceWidthPercentage = 0;
+    if (dimensions.height > 480) {
+      reduceHeightPercentage = 100 - ((dimensions.height - 480) / dimensions.height) * 100;
+    }
+    if (dimensions.width > 425) {
+      reduceWidthPercentage = 100 - ((dimensions.width - 425) / dimensions.width) * 100;
+    }
+    var reduce = Math.floor(reduceHeightPercentage < reduceWidthPercentage ? reduceHeightPercentage : reduceWidthPercentage);
+    if (reduce > 0) {
+      console.log("Original width: " + dimensions.width + ". Original height: " + dimensions.height);
+      console.log("create video thumb with percentage:", reduce);
+      const mp4FilePathThumb = path.normalize(path.join(fileDir, fileName.replace(/\.[^/.]+$/, '') + '_thumb_output.mp4'));
+      const targetTempFilePathThumb = path.join(os.tmpdir(), mp4FilePathThumb);
+      await convertFile(tempLocalFile, targetTempFilePathThumb, reduce + '%');
+      await bucket.upload(targetTempFilePathThumb, {
+        destination: mp4FilePathThumb,
+        uploadType: 'media',
+        resumable: false,
+        public: true,
+        metadata: { gzip: true, cacheControl: "public, max-age=31536000" }
+      });
+      fs.unlinkSync(targetTempFilePathThumb);
+    }
+
     fs.unlinkSync(tempLocalFile);
   }
 });
 
 function getCodec(filePath) {
+  console.log("Begin get codec of: " + filePath);
   return new Promise((resolve, reject) => {
     ffmpeg.ffprobe(filePath, (err, metadata) => {
       if (err) {
@@ -130,13 +159,29 @@ function getCodec(filePath) {
   })
 }
 
+function getDimentions(media) {
+  console.log("Getting Dimentions from:", media);
+  return new Promise((res, rej) => {
+    ffmpeg.ffprobe(media, (err, metadata) => {
+      if (err) {
+        console.log("Error occured while getting dimensions of: ", err);
+        rej(err);
+      }
+      res({
+        width: metadata.streams[0].width,
+        height: metadata.streams[0].height,
+      });
+    });
+  });
+}
 
-function convertFile(input, output) {
+function convertFile(input, output, size) {
   return new Promise((resolve, reject) => {
-    //console.log("Entering converting file");
+    console.log("Entering converting file: " + size);
     ffmpeg(input)
       .format("mp4")
       .videoCodec("libx264")
+      .size(size)
       .on('error', (err) => {
         console.log("Error in converting file");
         reject(err);
